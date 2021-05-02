7aecf41a5c4b1236dfa5f5672adc35cff26fe0b6
allipo <pestotnikall@yahoo.com>
2021-05-02 17:15:28 +0900

    changed logotype

--- sketch.js
+++ sketch.js
@@ -2,10 +2,9 @@ let stars = Array(200).fill(0);
 function setup() {
   createCanvas(innerWidth, innerHeight);
   stars = stars.map((s) => new Star());
-  // background(0, 0, 30);
 }
 function draw() {
-  background(0, 0, 20);
+  background(0, 0, 15);
   stars.forEach((s) => {
     s.create();
     s.move()
