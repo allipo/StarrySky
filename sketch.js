6236a1dcd668e07c45a63dc08e6432bc3de00ba2
allipo <pestotnikall@yahoo.com>
2021-05-02 16:42:43 +0900

    Added title and byline

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
-let stars = Array(100).fill(0)
+let stars = Array(200).fill(0);
 function setup() {
-  createCanvas(500, 500);
-  stars = stars.map(s=>new Star())
+  createCanvas(innerWidth, innerHeight);
+  stars = stars.map((s) => new Star());
   // background(0, 0, 30);
-
 }
 function draw() {
-  background(0, 0, 30);
-  stars.forEach(s=>{
-    s.create()
-    // s.move()
-  })
+  background(0, 0, 20);
+  stars.forEach((s) => {
+    s.create();
+    s.move()
+  });
 }
